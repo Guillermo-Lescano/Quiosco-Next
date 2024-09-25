c39816822b3d73b33f1bc4e4e387dc4410857fdf
Guillermo Lescano <guillelescano.gl@gmail.com>
2024-09-25 01:38:28 -0300

    dataFetching de categories y se agrego una funcionalidad para que se cierren los prismaClient

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,6 +1,14 @@
+import { prisma } from "@/src/lib/prisma";
+
 type Props = {};
 
-const OrderSidebar = (props: Props) => {
+async function getCategories() {
+  return await prisma.category.findMany(); //el findMany es para traer todos los registros
+}
+
+const OrderSidebar = async (props: Props) => {
+  const categories = await getCategories();
+  console.log(categories);
   return <aside className="md:w-72 md:h-screen bg-white">Order Aside</aside>;
 };
 
