c0a23028a652520d23e6becd1ed5749c704f975d
Guillermo Lescano <guillelescano.gl@gmail.com>
2025-03-26 23:39:31 -0300

    iterando sobre productos y formateando el precio a USD

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/products/ProductCard";
 import { prisma } from "@/src/lib/prisma";
 
 type Props = {};
@@ -19,7 +20,15 @@ export default async function OrderPage({
   params: { category: string };
 }) {
   const products = await getProducts(params.category);
-  console.log(products);
-  //con los params lo que ahcemos es leer el parametro category que esta en la url, osea es el dato pasado por query
-  return <div>Order Page</div>;
+
+  //con los params lo que HAcemos es leer el parametro category que esta en la url, osea es el dato pasado por query
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 }
